fd761dae543a8072da4cf77d5c93b3f5d664851e
xiaoxi-s <77871407@qq.com>
2022-03-15 01:18:40 +0000

    refractor so that the page wait for fetch a random postw

--- src/components/RandomPage.js
+++ src/components/RandomPage.js
@@ -2,28 +2,70 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
-const getRandomPost = (setPosts) => {
-  try {
+// const RandomPage = (info) => {
+//   const [posts, setPosts] = useState([]);
+//   useEffect(() => {
+//     return () => {
+//       setPosts([]);
+//     };
+//   }, []);
+  
+//   setTimeout(() => {
+//     RandomPage()
+//   }, 1000);
+//   return  posts.length > 0 ? (
+//     <Navigate to={"/posts/" + posts[0].postID}></Navigate>
+//   ) : ( 
+//     <Navigate to={"/"}></Navigate>
+//   );
+// };
+
+
+class RandomPage extends React.Component {
+  _isMounted = false
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    this._isMounted = true
+    this.getRandomPost = this.getRandomPost.bind(this)
+    this.getRandomPost()
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+  }
+
+  getRandomPost() {
     fetch(process.env.REACT_APP_REQUEST_URI + "/random-post")
       .then((response) => response.json())
       .then((data) => {
-        setPosts([data]);
+        console.log(data)
+        if (this._isMounted) {
+          this.setState({posts: [data]})
+        }
       });
-  } catch (e) {
-    setPosts([]);
   }
-};
 
-const RandomPage = (info) => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    getRandomPost(setPosts);
-  }, []);
-  return posts.length > 0 ? (
-    <Navigate to={"/posts/" + posts[0].postID}></Navigate>
-  ) : ( 
-    <Navigate to={"/"}></Navigate>
-  );
-};
+  render() {
+    const posts = this.state.posts
+    return (
+      <div>
+      {
+        posts.length > 0 && 
+        <Navigate to={"/posts/" + posts[0].postID}></Navigate>
+      }
+      {
+        posts.length == 0 &&
+        "Waiting..."
+      }
+      </div>
+    )
+  }
+}
 
 export default RandomPage;
